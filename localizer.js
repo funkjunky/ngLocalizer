9faa1767b84485bec3da2d48d225f2ea7a86f847
Jason McCarrell <jason.abz@gmail.com>
2014-05-20 17:23:49 -0400

    First commit

---
+++ localizer.js
@@ -0,0 +1,23 @@
+var app = angular.module('ngLocalization', ['ngCookies']);
+
+app.factory('Localizer', function($cookies) {
+  var dictionary = {};
+  return {
+    setDictionary: function(aDictionary) {
+      dictionary = aDictionary;
+    },
+    phrase: function(phrase) {
+      var language = $cookies.language.split('-')[0];
+      if(dictionary[phrase])
+        return dictionary[phrase][language] || phrase;
+      else
+        return phrase;
+    },
+  };
+});
+
+app.filter('localize', function(Localizer) {
+  return function(input) {
+    return Localizer.phrase(input);
+  };
+});
